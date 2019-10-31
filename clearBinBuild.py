1b4bc45453f26eeb127503664f8b47e87a54a98a
Oleksandr Brodskyi <abrodskyi@luxoft.com>
2019-10-31 13:13:45 +0200

    remove build folder

--- clearBinBuild.py
+++ clearBinBuild.py
@@ -1,3 +1,20 @@
 import shutil
+import os
 
+currentDir = os.getcwd()
+buildDir = os.path.join(currentDir, "build")
+binDir = os.path.join(currentDir, "bin")
+
+#shutil.rmtree(buildDir)
+#shutil.rmtree(binDir)
+
+def remove_dir_contents(dir_path):
+    for root, dirs, files in os.walk(dir_path):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
+
+remove_dir_contents(buildDir)
+remove_dir_contents(binDir)
 
